3c1cad6a1b18c34f4b37c4890793ebedd346c530
BahruzAlizada <behruzelzadee@gmail.com>
2023-07-28 15:05:09 +0400

    Autofac

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,17 +1,18 @@
-﻿using BusinessLayer.Abstract;
+﻿using Autofac.Extensions.DependencyInjection;
+using BusinessLayer.Abstract;
 using BusinessLayer.Concrete;
 using DataAccessLayer.Abstract;
 using DataAccessLayer.Concrete.EntityFramework;
 
 var builder = WebApplication.CreateBuilder(args);
+Host.CreateDefaultBuilder(args).UseServiceProviderFactory(new AutofacServiceProviderFactory());
 
 // Add services to the container.
 //Autofac,Ninject,CastleWindsor,StructoreMap,LightInject,DryInject-->IoC Container
 //AOP-Bir Metodun əvvəlində axrında çalışan kod parçalarıdır.
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<IProductService,ProductManager>(); //1 dənə arxa planda ProductManager yaradır.
-builder.Services.AddSingleton<IProductDal, EFProductDal>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
